c061d848c02772576b40bf241dd27df3f19be486
Greg Ogun <gregogun97@gmail.com>
2020-08-01 18:40:04 +0100

    Create and style page

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const prevButton = document.querySelector('.carousel__navButton--prev');
+const nextButton = document.querySelector('.carousel__navButton--next');
+const textHeading = document.querySelector('.carousel__text--heading');
+const textBody = document.querySelector('.carousel__text--body');
+const priceOld = document.querySelector('.carousel__price--old');
+const priceNew = document.querySelector('.carousel__price--new');
+
+
+nextButton.addEventListener('click', () => {
+    console.log('clicked');
+});
